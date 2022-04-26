346c49f1171e9b56408c1f7b86c0238c4cb5cb28
aykut <yourgitemail@example.com>
2022-04-26 16:06:01 +0200

    update valide validate number

--- JS/fortuneTeller/script.js
+++ JS/fortuneTeller/script.js
@@ -15,25 +15,41 @@ let partner = ['Bellamy',
 let job = ['Aerospace Engineering and Operations Technicians',
    ' Aerospace Engineers',
     'Agricultural Engineers',
-    'Architects, Except Landscape and Naval',
-   ' Architectural and Civil Drafters',
+    'Architect',
     'Architectural Drafters',
     'Automotive Engineering Technicians',
    ' Automotive Engineers',
     ];
 let loc = ['New York City','Los Angeles','Chicago'];
-let children = [0,1,2,3];
+let children = [2,3,4];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-console.log(`Your partner is ${partner[getRandomInt(partner.length)]}`)
+  function FortuneTeller (partner,job,loc,children){
+    this.partner = partner;
+    this.job = job;
+    this.loc = loc;
+    this.children = children;
+    this.yourFortune = function(){
+      return `You will be a ${this.job} in ${this.loc}, and married to ${this.partner} with ${this.children} kids.`;
+    }
+  };
 
-console.log(`Your job is  ${job[getRandomInt(job.length)]}`)
+let fortune = new FortuneTeller(partner[getRandomInt(partner.length)],job[getRandomInt(job.length)],loc[getRandomInt(loc.length)],children[getRandomInt(children.length)]);
+console.log(fortune.yourFortune());
 
-console.log(`Your location is  ${loc[getRandomInt(loc.length)]}`)
 
-let futureChildren = children[getRandomInt(children.length)]
 
-console.log(`Your children is  ${futureChildren} ${futureChildren > 1 ? 'Kids': 'Kid'}`)
+
+
+// console.log(`Your partner is ${partner[getRandomInt(partner.length)]}`)
+
+// console.log(`Your job is  ${job[getRandomInt(job.length)]}`)
+
+// console.log(`Your location is  ${loc[getRandomInt(loc.length)]}`)
+
+// let futureChildren = children[getRandomInt(children.length)]
+
+// console.log(`Your children is  ${futureChildren} ${futureChildren > 1 ? 'Kids': 'Kid'}`)
