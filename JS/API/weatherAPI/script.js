49ed9403e0651e4110945940f9633b0eeec15889
maykicn <aykut.can@powercoders.org>
2022-05-25 19:05:24 +0200

    country and cities options are added and wheater information of the city are got

--- JS/API/weatherAPI/script.js
+++ JS/API/weatherAPI/script.js
@@ -1,56 +1,70 @@
-const cities = document.getElementById("cities");
+const weatherResult = document.getElementById("weather");
+const countyOption = document.getElementById('country')
+const citySelect = document.getElementById('city')
+var countryOfWeahter;
+var cityOfWeahter;
 var lat;
 var lon;
 
 
 window.onload = async () => {
 
-
-     // to get city
-
+     //to get name of the country
      let sehirler = await (await fetch('https://countriesnow.space/api/v0.1/countries')).json();
-     console.log(`sehirler.data[0].iso3 ${sehirler.data[0].iso3}`)
+     
+     //create all country for options 
+
+     for(const nameOfCountry of sehirler.data){
+        const country = document.createElement('option');
+        country.value = nameOfCountry.iso3;
+        country.text = nameOfCountry.iso3;
+        countyOption.appendChild(country)
+     }
  
- 
- 
-     // for (const country of sehirler.data) {
- 
-     //     console.log(country)
-     //     for (const city of country.cities) {
- 
-     //         console.log(city)
-             
-     //     }
-     //     console.log('!!!!!!control!!!!')
-         
-     // }
- 
-
-
-
-    // to get lat and lon value
-    let result = await (await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Paris,FRA&limit=1&appid=3de1bbb1b94e972a8699a03868e73d3d')).json();
-    console.log(result[0])
-    lat = result[0].lat;
-    lon = result[0].lon;
-    console.log(`London sehrinin lat degeri: ${lat}, lon degeri: ${lon}`)
-    cities.innerText =`Paris sehrinin lat degeri: ${lat}, lon degeri: ${lon}`;
-
-
-
-
-    // to get weather
-    // let weather = await (await fetch('https://api.openweathermap.org/data/2.5/weather?lat=48.8588897&lon=2.3200410217200766&appid=3de1bbb1b94e972a8699a03868e73d3d')).json();
-    let weather = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3de1bbb1b94e972a8699a03868e73d3d`)).json();
-
-    console.log(weather.weather[0].description)
-
-
-
-
-
-
-   
-    
     
 }
+
+
+ //selected counntry and city from country dropdownlists
+  countyOption.addEventListener('change', async ()=>{
+    let sehirler = await (await fetch('https://countriesnow.space/api/v0.1/countries')).json();
+
+    //getting from country selected value from country dropdownlist
+    var value = countyOption.options[countyOption.selectedIndex].value;
+    countryOfWeahter = value;
+    
+
+    //getting cities according to selected value from country dropdownlist
+    for (let index = 0; index < sehirler.data.length; index++) {
+        if (value == sehirler.data[index].iso3){
+            for(city of sehirler.data[index].cities ){
+                cityOfWeahter = city;
+                const cityOptions = document.createElement('option');
+                cityOptions.value = city;
+                cityOptions.text = city;
+                citySelect.appendChild(cityOptions)
+
+            }
+
+        }
+        
+    }
+
+    
+  })
+
+  citySelect.addEventListener('change',async ()=>{
+
+    var value = citySelect.options[citySelect.selectedIndex].value;
+    cityOfWeahter = value;
+
+    let result = await (await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityOfWeahter},${countryOfWeahter}&limit=1&appid=3de1bbb1b94e972a8699a03868e73d3d`)).json();
+    lat = result[0].lat;
+    lon = result[0].lon;
+    let weather = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3de1bbb1b94e972a8699a03868e73d3d`)).json();
+    weatherResult.innerText = `Country:${countryOfWeahter} , city: ${cityOfWeahter}, lat: ${lat}, lon:${lon}, weather: ${weather.weather[0].description}`;
+
+    
+
+  })
+ 
